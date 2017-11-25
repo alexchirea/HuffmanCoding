acb6bb8d130d1d4476cc13fd51ae8e018ba705d6
Alex Chirea <alexchirea11@gmail.com>
2017-11-25 14:24:06 +0200

    Declarare [func codificare()]
    - citire din fisier
    - determinare nr litere
    - determinare nr de aparitii al fiecarui caracter

---
+++ codificare.h
@@ -0,0 +1,30 @@
+//
+// Created by Alexandru Chirea on 25.11.2017.
+//
+#include <string.h>
+
+#ifndef HUFFMANCODING_CODIFICARE_H
+#define HUFFMANCODING_CODIFICARE_H
+
+#endif //HUFFMANCODING_CODIFICARE_H
+
+int min(int a, int b) {
+    return (a>b)?b:a;
+}
+
+int max(int a, int b) {
+    return (a<b)?b:a;
+}
+
+void codificare (FILE *fisier, char* intrare, int* frecv, int *nrcar, int *start, int *stop) {
+    int i;
+    while (fgets(intrare, sizeof(intrare), fisier)) {
+        for (i=0; i<strlen(intrare); i++) {
+            frecv[intrare[i]]++;
+            *start = min(*start,intrare[i]);
+            *stop = max(*stop,intrare[i]);
+            (*nrcar)++;
+        }
+    }
+    fclose(fisier);
+}
--- main.c
+++ main.c
@@ -1,10 +1,31 @@
 #include <stdio.h>
+#include "codificare.h"
 
 //Creat de Alexandru-Cornel Chirea
 //Univ. Bucuresti, Facultatea de Matematica si Informatica
 //CTI - Gr. 154
 
+char intrare[1001];
+int frecv[300], nrcar, start=9999, stop;
 int main() {
-    printf("Hello, World!\n");
+
+    //Fisierul din care se citeste textul de (de)codificat.
+    FILE *f = fopen("huffman.txt","r");
+
+    int optiune; //1 codificare sau 2 decodificare
+
+    printf("Introduceti 1 pentru codificare sau 2 pentru decodificare, dupa care apasati tasta Enter.\nOptiune = ");
+    //scanf("%d", &optiune);
+    optiune=1;
+    switch (optiune) {
+        case 1:
+            codificare(f, intrare, frecv, &nrcar, &start, &stop);
+            break;
+        case 2:
+            //decodificare
+            break;
+        default:
+            printf("\nEROARE: Optiunea introdusa nu exista!\n");
+    }
     return 0;
 }
