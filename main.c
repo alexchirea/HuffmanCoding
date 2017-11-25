d5963b1f446bc02d1addf185a19d77cd2f51c621
Alex Chirea <alexchirea11@gmail.com>
2017-11-25 12:45:18 +0200

    Author info

--- main.c
+++ main.c
@@ -1,5 +1,9 @@
 #include <stdio.h>
 
+//Creat de Alexandru-Cornel Chirea
+//Univ. Bucuresti, Facultatea de Matematica si Informatica
+//CTI - Gr. 154
+
 int main() {
     printf("Hello, World!\n");
     return 0;
