257e8771da1dd0fd95e89a56798e4f2eea62a8aa
Alex Chirea <alexchirea11@gmail.com>
2017-12-27 12:45:57 +0200

    Backup
    There are some memory leak problems.

--- codificare.h
+++ codificare.h
@@ -10,9 +10,7 @@
 
 void codificare(FILE *fisier) {
     int i, frecv[500] = {0}, nrcar = 0, start = 9999, stop = 0, k = 0;
-
     char intrare[1001];
-
     while (fgets(intrare, sizeof(intrare), fisier)) {
         for (i = 0; i < strlen(intrare); i++) {
             frecv[intrare[i]]++;
@@ -21,19 +19,14 @@ void codificare(FILE *fisier) {
             nrcar++;
         }
     }
-
     fclose(fisier);
-
     for (i = start; i <= stop; i++) {
         if (frecv[i]) {
             k++;
         }
     }
-
     listaNoduri = (struct Nod **) malloc(sizeof(struct Nod *) * k);
-
     k = 0;
-
     for (i = start; i <= stop; i++) {
         if (frecv[i]) {
             struct Nod *nodNou = (struct Nod *) malloc(sizeof(struct Nod));
@@ -43,20 +36,30 @@ void codificare(FILE *fisier) {
             k++;
         }
     }
-
-    //printf("%d", k);
-
-    listaNoduri = sortare(listaNoduri, k - 1);
-    //Afisare frecvente
-    printf("\n");
-    for (i = 0; i < k; i++) {
-        printf("%c\t", listaNoduri[i]->caracter);
+    //struct Nod *root = (struct Nod *) malloc(sizeof(struct Nod));
+    sortare(listaNoduri, k - 1);
+    //for (i=0; i<k; i++) printf("%d:'%c' ",listaNoduri[i]->valoare,listaNoduri[i]->caracter);
+    int o = k;
+    while (o) {
+        struct Nod *nodNou = (struct Nod *) malloc(sizeof(struct Nod));
+        nodNou->valoare = listaNoduri[0]->valoare + listaNoduri[1]->valoare;
+        nodNou->st = listaNoduri[0];
+        //printf("%p\n",listaNoduri[0]);
+        nodNou->dr = listaNoduri[1];
+        //printf("%p\n",listaNoduri[1]);
+        //root = nodNou;
+        listaNoduri[0] = nodNou;
+        o--;
+        if (o == 1) {
+            break;
+        }
+        for (i = 1; i < o; i++) {
+            listaNoduri[i] = listaNoduri[i + 1];
+        }
+        sortare(listaNoduri, o - 1); // ??????
     }
-    printf("\n");
-    for (i = 0; i < k; i++) {
-        printf("%d\t", listaNoduri[i]->valoare);
-    }
-
+    parcurgere(listaNoduri[0]);
+    free(listaNoduri);
 }
 
 #endif HUFFMANCODING_CODIFICARE_H
--- definitii.h
+++ definitii.h
@@ -1,7 +1,6 @@
 //
 // Created by Alexandru Chirea on 18.12.2017.
 //
-
 #ifndef HUFFMANCODING_DEFINITII_H
 #define HUFFMANCODING_DEFINITII_H
 
@@ -19,25 +18,43 @@ struct Nod {
     struct Nod *st;
     struct Nod *dr;
 };
-
 struct Nod **listaNoduri;
 
 void adaugare();
 
-struct Nod **sortare(struct Nod **lista, int k) {
+void sortare(struct Nod **lista, int k) {
     int i, o = 0;
     struct Nod *aux;
     while (o == 0) {
         o = 1;
         for (i = 0; i < k; i++) {
             if (lista[i]->valoare > lista[i + 1]->valoare) {
+                o = 0;
                 aux = lista[i];
                 lista[i] = lista[i + 1];
                 lista[i + 1] = aux;
+            } else if (lista[i]->valoare == lista[i + 1]->valoare) {
+                if (lista[i]->caracter > lista[i + 1]->caracter) {
+                    o = 0;
+                    aux = lista[i];
+                    lista[i] = lista[i + 1];
+                    lista[i + 1] = aux;
+                }
             }
         }
     }
-    return lista;
 }
 
-#endif //HUFFMANCODING_DEFINITII_H
+void parcurgere(struct Nod *nod) {
+    if (nod->st != NULL) {
+        printf("%d %c\n", nod->valoare, nod->caracter);
+        parcurgere(nod->st);
+        parcurgere(nod->dr);
+        free(nod);
+    } else {
+        printf("%d %c\t", nod->valoare, nod->caracter);
+        free(nod);
+    }
+}
+
+#endif HUFFMANCODING_DEFINITII_H
